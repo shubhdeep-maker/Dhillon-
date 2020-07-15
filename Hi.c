ed947d3cc7c677b0f98b3eb076e3e4399c633384
shubhdeep-maker <68057789+shubhdeep-maker@users.noreply.github.com>
2020-07-15 16:27:39 +0530

    Created Hi.c
    This file hepls to access array elements using pointer.

---
+++ Hi.c
@@ -0,0 +1,18 @@
+//ACCESS ARRAY ELEMENTS USING POINTER
+#include<iostream>
+using namespace std;
+int main()
+{
+    int data[5];
+    cout<<“Enter Elements: “;
+    
+    for(int i=0;i<5;i++){
+        cin>>data[i];
+    }
+    
+    cout<<“You entered: “;
+    for(int i=0;i<5;i++){
+        cout<<endl<<*(data+i);
+    }
+    return 0;
+}
